b91a5c71044a210854c68e97b41e27e13e8a12a8
Azoxi78 <azoxi42@gmail.com>
2025-04-21 08:30:50 +0200

    fff

---
+++ KITTFFA-ZYRO/Azox/Main.php
@@ -0,0 +1,27 @@
+<?php 
+
+naemspace Azox;
+
+use pocketmine\plugin\PluginBase;
+use Azox\Task\TimeTask;
+
+class Main extends PluginBase {
+
+  public function onEnable(): void {
+
+    $this->getLogger()->info("ZyroFFA activé");
+    $this->saveDefaultConfig();
+    $this->getScheduler()->scheduleRepeatingTask(new TimeTask(), 20 * 60);
+
+
+
+  }
+    public function onDisable(): void {
+
+        $this->getLogger()->info("ZyroFFA desaactivé");
+ }
+
+
+
+
+}
